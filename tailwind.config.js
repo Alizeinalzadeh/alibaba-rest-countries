589e3db643a46ba044a9a7c50ff7799cba9dfc6f
alizeinalzadeh <ali94zeinalzadeh@gmail.com>
2023-04-06 13:33:30 +0330

    [🔧] add styles colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: 'class',
 	content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dark: {
+					background: 'hsl(207, 26%, 17%)',
+					primary: 'hsl(209, 23%, 22%)',
+					text: 'hsl(0, 0%, 100%)',
+				},
+				light: {
+					background: 'hsl(0, 0%, 98%)',
+					primary: 'hsl(0, 0%, 100%)',
+					text: 'hsl(200, 15%, 8%)',
+					input: 'hsl(0, 0%, 52%)',
+				},
+			},
+			fontFamily: {
+				sans: ['var(--font-nunito)', ...fontFamily.nunito],
+			},
+		},
 	},
 	plugins: [],
 };
