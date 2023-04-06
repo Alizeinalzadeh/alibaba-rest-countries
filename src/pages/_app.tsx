918a61956b2c2ba981b937c534d2090a4e497630
alizeinalzadeh <ali94zeinalzadeh@gmail.com>
2023-04-06 13:33:56 +0330

    [✨] add fonts and icons globally

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Nunito_Sans } from 'next/font/google';
 import Head from 'next/head';
+import Script from 'next/script';
+
+const nunitoSans = Nunito_Sans({
+	subsets: ['latin'],
+	variable: '--font-nunito',
+	weight: ['300', '600', '800'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
@@ -11,7 +19,19 @@ export default function App({ Component, pageProps }: AppProps) {
 					content='width=device-width, initial-scale=1'
 				/>
 			</Head>
-			<Component {...pageProps} />
+			<main className={`${nunitoSans.variable} font-nunito`}>
+				<Component {...pageProps} />
+			</main>
+			<Script
+				type='module'
+				src='https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js'
+				strategy='afterInteractive'
+			/>
+			<Script
+				noModule
+				src='https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js'
+				strategy='afterInteractive'
+			/>
 		</>
 	);
 }
