98e9838041c9cc7ea76fb5a0f27db30eb8fb34f9
alizeinalzadeh <ali94zeinalzadeh@gmail.com>
2023-04-07 12:44:06 +0330

    [🔧] add dark mode data value to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-	darkMode: 'class',
+	darkMode: ['class', '[theme-mode="dark"]'],
 	content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
