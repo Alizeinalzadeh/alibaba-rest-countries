c6598227fd8e714e0819296a64525744acefd42d
alizeinalzadeh <ali94zeinalzadeh@gmail.com>
2023-04-07 20:22:08 +0330

    [✨] read country details and names

--- src/adapter/HandleCountries.ts
+++ src/adapter/HandleCountries.ts
@@ -2,11 +2,28 @@ import { BaseAxiosService } from './BaseAxiosService';
 
 export class HandleCountries extends BaseAxiosService {
 	readCountries = (contoller?: AbortSignal) => {
-		return this._get(`all?fields=name,flags,population,region,capital`, { signal: contoller });
+		return this._get(`all?fields=name,flags,population,region,capital,cca3`, {
+			signal: contoller,
+		});
 	};
 
 	searchCountries = (query: string, controller?: AbortSignal) => {
 		return this._get(`name/${query}?fields=name,flags,population,region,capital`, {
+			signal: controller,
+		});
+	};
+
+	readCountry = (code: string, controller?: AbortSignal) => {
+		return this._get(
+			`alpha/${code}?fields=name,flags,population,region,subregion,capital,tld,currencies,languages,borders`,
+			{
+				signal: controller,
+			}
+		);
+	};
+
+	readCountryName = (code: string, controller?: AbortSignal) => {
+		return this._get(`alpha/${code}?fields=name,cca3`, {
 			signal: controller,
 		});
 	};
